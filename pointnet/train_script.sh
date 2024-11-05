92ae03e8b349786e75498c74c1bc0e90fb27255b
ZhangIceNight <a597536806@163.com>
2024-11-05 18:50:45 +0800

    Update at 2024-11-05 18:50:45

---
+++ pointnet/train_script.sh
@@ -0,0 +1,9 @@
+python train_semseg.py \
+    --model pointnet_sem_seg \
+    --batch_size 16 \
+    --npoint 4096 \
+    --epoch 128 \
+    --learning_rate 0.001 \
+    --optimizer Adam \
+    --log_dir pointnet_leaf_seg \
+    --gpu 0
