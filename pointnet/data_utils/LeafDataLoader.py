7dbbe2f7bce92aedc4863877a5e0baa9ec627148
ZhangIceNight <a597536806@163.com>
2024-11-05 16:42:36 +0800

    .

--- pointnet/data_utils/LeafDataLoader.py
+++ pointnet/data_utils/LeafDataLoader.py
@@ -17,19 +17,15 @@ class LeafDatasetWholeScene(Dataset):
         
         # 读取文件列表
         list_filename = 'trainval_list.txt' if split == 'train' else 'test_list.txt'
-        with open(os.path.join(root, 'leaf_dataset', list_filename), 'r') as f:
-            self.file_list = [line.strip().replace('.txt', '.npy') for line in f.readlines()]
-            
-        self.scene_points_list = []
-        self.semantic_labels_list = []
+        with open(os.path.join(root, list_filename), 'r') as f:
+            self.file_list = [line.strip() for line in f.readlines()]
         
         # 加载所有场景数据
         for file in self.file_list:
             # 使用npy格式加载数据
-            data = np.load(os.path.join(root, 'npy_data', file))
-            points = data[:, :3]  # XYZ
+            data = np.load(os.path.join(root, file))
+            points = data[:, :3]  # XYZ 
             labels = data[:, -1]  # 标签
-            
             points = pc_normalize(points)
             self.scene_points_list.append(points)
             self.semantic_labels_list.append(labels)
