d18d95a6a9e1599b02a93c628bdc5dbc05fd68c7
ZhangIceNight <a597536806@163.com>
2024-11-05 18:57:28 +0800

    Update at 2024-11-05 18:57:28

--- pointnet/train_semseg.py
+++ pointnet/train_semseg.py
@@ -47,7 +47,6 @@ def parse_args():
     parser.add_argument('--npoint', type=int, default=4096, help='Point Number [default: 4096]')
     parser.add_argument('--step_size', type=int, default=10, help='Decay step for lr decay [default: every 10 epochs]')
     parser.add_argument('--lr_decay', type=float, default=0.7, help='Decay rate for lr decay [default: 0.7]')
-    parser.add_argument('--test_area', type=int, default=5, help='Which area to use for test, option: 1-6 [default: 5]')
 
     return parser.parse_args()
 
