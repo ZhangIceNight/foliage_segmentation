dd4e484a79a030c08ef5d309b2b72925af809f14
ZhangIceNight <a597536806@163.com>
2024-11-06 14:57:41 +0800

    Update at 2024-11-06 14:57:40

---
+++ pointnet/data_utils/augment_script.sh
@@ -0,0 +1,5 @@
+python prepare_augmented_data.py \
+    --data_dir /public/wjzhang/datasets/LabelledPCnpy \
+    --output_dir /public/wjzhang/datasets/LabelledPCnpy_augmented \
+    --train_ratio 0.8 \
+    --seed 42
