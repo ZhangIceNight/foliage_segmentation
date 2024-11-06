f74174d98eb8bea53ae2abf1c886566fd876f933
ZhangIceNight <a597536806@163.com>
2024-11-06 12:29:10 +0800

    Update at 2024-11-06 12:29:09

--- pointnet/models/pointnet2_sem_seg.py
+++ pointnet/models/pointnet2_sem_seg.py
@@ -1,15 +1,17 @@
+import torch
 import torch.nn as nn
+import torch.nn.parallel
+import torch.utils.data
 import torch.nn.functional as F
-from models.pointnet2_utils import PointNetSetAbstraction,PointNetFeaturePropagation
-
+from pointnet2_utils import PointNetSetAbstraction, PointNetFeaturePropagation
 
 class get_model(nn.Module):
     def __init__(self, num_classes):
         super(get_model, self).__init__()
         self.sa1 = PointNetSetAbstraction(1024, 0.1, 32, 3, [32, 32, 64], False)
-        self.sa2 = PointNetSetAbstraction(256, 0.2, 32, 3, [64, 64, 128], False)
-        self.sa3 = PointNetSetAbstraction(64, 0.4, 32, 3, [128, 128, 256], False)
-        self.sa4 = PointNetSetAbstraction(16, 0.8, 32, 3, [256, 256, 512], False)
+        self.sa2 = PointNetSetAbstraction(256, 0.2, 32, 64, [64, 64, 128], False)
+        self.sa3 = PointNetSetAbstraction(64, 0.4, 32, 128, [128, 128, 256], False)
+        self.sa4 = PointNetSetAbstraction(16, 0.8, 32, 256, [256, 256, 512], False)
         self.fp4 = PointNetFeaturePropagation(768, [256, 256])
         self.fp3 = PointNetFeaturePropagation(384, [256, 256])
         self.fp2 = PointNetFeaturePropagation(320, [256, 128])
@@ -20,8 +22,8 @@ class get_model(nn.Module):
         self.conv2 = nn.Conv1d(128, num_classes, 1)
 
     def forward(self, xyz):
-        l0_points = xyz
         l0_xyz = xyz[:,:3,:]
+        l0_points = None
 
         l1_xyz, l1_points = self.sa1(l0_xyz, l0_points)
         l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
@@ -33,19 +35,18 @@ class get_model(nn.Module):
         l1_points = self.fp2(l1_xyz, l2_xyz, l1_points, l2_points)
         l0_points = self.fp1(l0_xyz, l1_xyz, None, l1_points)
 
-        x = self.drop1(F.relu(self.bn1(self.conv1(l0_points))))
+        x = self.drop1(F.relu(self.bn1(self.conv1(l0_points)), inplace=True))
         x = self.conv2(x)
         x = F.log_softmax(x, dim=1)
         x = x.permute(0, 2, 1)
-        return x
-
+        return x, l4_points
 
 class get_loss(nn.Module):
     def __init__(self):
         super(get_loss, self).__init__()
-    def forward(self, pred, target, trans_feat, weight):
-        total_loss = F.nll_loss(pred, target, weight=weight)
 
+    def forward(self, pred, target):
+        total_loss = F.nll_loss(pred, target)
         return total_loss
 
 if __name__ == '__main__':
