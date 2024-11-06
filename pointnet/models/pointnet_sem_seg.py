50e1100b2ff7f224f8e784b45e9c94960ba43564
ZhangIceNight <a597536806@163.com>
2024-11-06 10:22:38 +0800

    Update at 2024-11-06 10:22:38

--- pointnet/models/pointnet_sem_seg.py
+++ pointnet/models/pointnet_sem_seg.py
@@ -3,14 +3,14 @@ import torch.nn as nn
 import torch.nn.parallel
 import torch.utils.data
 import torch.nn.functional as F
-from pointnet_utils import PointNetEncoder, feature_transform_reguliarzer
+from pointnet_utils import PointNetEncoder
 
 
 class get_model(nn.Module):
     def __init__(self, num_class):
         super(get_model, self).__init__()
         self.k = num_class
-        self.feat = PointNetEncoder(global_feat=False, feature_transform=True, channel=3)
+        self.feat = PointNetEncoder(global_feat=False, channel=3)
         self.conv1 = torch.nn.Conv1d(1088, 512, 1)
         self.conv2 = torch.nn.Conv1d(512, 256, 1)
         self.conv3 = torch.nn.Conv1d(256, 128, 1)
@@ -22,7 +22,7 @@ class get_model(nn.Module):
     def forward(self, x):
         batchsize = x.size()[0]
         n_pts = x.size()[2]
-        x, trans, trans_feat = self.feat(x)
+        x = self.feat(x)
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
@@ -30,18 +30,16 @@ class get_model(nn.Module):
         x = x.transpose(2,1).contiguous()
         x = F.log_softmax(x.view(-1,self.k), dim=-1)
         x = x.view(batchsize, n_pts, self.k)
-        return x, trans_feat
+        return x
+
 
 class get_loss(torch.nn.Module):
-    def __init__(self, mat_diff_loss_scale=0.001):
+    def __init__(self):
         super(get_loss, self).__init__()
-        self.mat_diff_loss_scale = mat_diff_loss_scale
 
-    def forward(self, pred, target, trans_feat, weight):
-        loss = F.nll_loss(pred, target, weight = weight)
-        mat_diff_loss = feature_transform_reguliarzer(trans_feat)
-        total_loss = loss + mat_diff_loss * self.mat_diff_loss_scale
-        return total_loss
+    def forward(self, pred, target, weight):
+        loss = F.nll_loss(pred, target, weight=weight)
+        return loss
 
 
 if __name__ == '__main__':
