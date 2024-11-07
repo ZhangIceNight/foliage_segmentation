01e258b358d289c58ea30a7145c4985c298c755f
ZhangIceNight <a597536806@163.com>
2024-11-07 10:25:25 +0800

    Update at 2024-11-07 10:25:25

--- DHCN/utils/get_projection2D.py
+++ DHCN/utils/get_projection2D.py
@@ -134,7 +134,7 @@ def projection(path, out_path):
         points = data[:, :3]
         labels = data[:, -1].astype(np.int32)
         file_name = file.split('.npy')[0]
-        g(points, labels, out_path, file_name)
+        camera_rotation(points, labels, out_path, file_name)
 
 
 def main(config):
