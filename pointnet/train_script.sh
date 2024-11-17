01b13e1184030a06291430fb4fd3369e4d55d2da
ZhangIceNight <a597536806@163.com>
2024-11-17 13:30:25 +0800

    Update at 2024-11-17 13:30:25

--- pointnet/train_script.sh
+++ pointnet/train_script.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0
 
 
-
+# pointnet
 # python train_semseg.py \
 #     --model pointnet_sem_seg \
 #     --batch_size 16 \
@@ -12,6 +12,7 @@ export CUDA_VISIBLE_DEVICES=0
 #     --log_dir pointnet_leaf_seg \
 #     --gpu 0
 
+# pointnet2
 # python train_semseg.py \
 #     --model pointnet2_sem_seg \
 #     --batch_size 16 \
@@ -21,13 +22,26 @@ export CUDA_VISIBLE_DEVICES=0
 #     --optimizer Adam \
 #     --log_dir pointnet2_leaf_seg_1 \
 #     --gpu 0
-    
+
+# # pt_mamba
+# python train_semseg.py \
+#     --model pt_mamba \
+#     --batch_size 16 \
+#     --npoint 4096 \
+#     --epoch 128 \
+#     --learning_rate 0.001 \
+#     --optimizer Adam \
+#     --log_dir pt_mamba_0 \
+#     --gpu 0
+
+# pt_hmamba
 python train_semseg.py \
-    --model pt_mamba \
+    --model pt_hmamba \
     --batch_size 16 \
     --npoint 4096 \
     --epoch 128 \
     --learning_rate 0.001 \
     --optimizer Adam \
-    --log_dir pt_mamba_0 \
+    --log_dir pt_hmamba_0 \
     --gpu 0
+
