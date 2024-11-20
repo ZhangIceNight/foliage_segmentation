140a2abe4ece9e98ac5be6b401ae4d2d4bb46f1b
ZhangIceNight <a597536806@163.com>
2024-11-20 18:20:12 +0800

    Update at 2024-11-20 18:20:12

--- pointnet/train_script.sh
+++ pointnet/train_script.sh
@@ -54,17 +54,17 @@ fi
 #     --gpu 0 \
 #     --ckpts 'pretrain.pth'
 
-# pt_hmamba_orderMultiply
-python train_semseg.py \
-    --model pt_hmamba_orderMultiply \
-    --batch_size 16 \
-    --npoint 4096 \
-    --epoch 300 \
-    --learning_rate 0.0002 \
-    --optimizer AdamW \
-    --log_dir "pt_hmamba_orderMultiply_$1" \
-    --gpu 0 \
-    --ckpts 'pretrain.pth'
+# # pt_hmamba_orderMultiply
+# python train_semseg.py \
+#     --model pt_hmamba_orderMultiply \
+#     --batch_size 16 \
+#     --npoint 4096 \
+#     --epoch 300 \
+#     --learning_rate 0.0002 \
+#     --optimizer AdamW \
+#     --log_dir "pt_hmamba_orderMultiply_$1" \
+#     --gpu 0 \
+#     --ckpts 'pretrain.pth'
 
 # # pt_hmamba_orderMultiply_static
 # python train_semseg.py \
@@ -113,3 +113,16 @@ python train_semseg.py \
 #     --log_dir "pt_mamba_random_$1" \
 #     --gpu 0 \
 #     # --ckpts 'pretrain.pth'
+
+
+# pt_hmamba_orderMultiply_center
+python train_semseg.py \
+    --model pt_hmamba_orderMultiply_center \
+    --batch_size 16 \
+    --npoint 4096 \
+    --epoch 300 \
+    --learning_rate 0.0002 \
+    --optimizer AdamW \
+    --log_dir "pt_hmamba_orderMultiply_center_$1" \
+    --gpu 0 \
+    --ckpts 'pretrain.pth'
