fd256c60f59f0fd7d975ef9408ae97356de4266b
ZhangIceNight <a597536806@163.com>
2024-11-24 14:17:50 +0800

    Update at 2024-11-24 14:17:50

--- pointnet/train_script_evo.sh
+++ pointnet/train_script_evo.sh
@@ -56,29 +56,29 @@ fi
 
 
 
-pt_mamba_random   
-python train_evo.py \
-    --model pt_mamba_random \
-    --batch_size 16 \
-    --npoint 4096 \
-    --epoch 300 \
-    --learning_rate 0.0002 \
-    --optimizer AdamW \
-    --log_dir "pt_mamba_random_$1" \
-    --gpu 0 \
-    --ckpts 'pretrain.pth'
-
-
-
-# # pct_seg
+# #pt_mamba_random   
 # python train_evo.py \
-#     --model pct_seg \
+#     --model pt_mamba_random \
 #     --batch_size 16 \
 #     --npoint 4096 \
 #     --epoch 300 \
 #     --learning_rate 0.0002 \
 #     --optimizer AdamW \
-#     --log_dir "pct_seg_$1" \
+#     --log_dir "pt_mamba_random_$1" \
 #     --gpu 0 \
+#     --ckpts 'pretrain.pth'
+
+
+
+# pct_seg
+python train_evo.py \
+    --model pct_seg \
+    --batch_size 16 \
+    --npoint 4096 \
+    --epoch 300 \
+    --learning_rate 0.0002 \
+    --optimizer AdamW \
+    --log_dir "pct_seg_$1" \
+    --gpu 0 \
 
  
