f9bc25a0dfcd5aa0ce3b27a978527f45ab40a54c
ZhangIceNight <a597536806@163.com>
2024-11-24 15:05:02 +0800

    Update at 2024-11-24 15:05:02

--- pointnet/data_utils/prepare_data_binary.py
+++ pointnet/data_utils/prepare_data_binary.py
@@ -15,9 +15,14 @@ def convert_txt_to_npy(data_dir, output_dir):
         
         # 读取txt数据
         data = np.loadtxt(input_path, delimiter=None, skiprows=1)
-        
-        # 保存为npy格式
+        data = data[:, [0,1,2,4]]
+        labels = data[:, 3]
+        if not np.all(np.isin(labels, [1, 2, 3])):
+            raise ValueError(f"发现非法标签值，标签值必须是1、2或3")
+            
+        data[:, 3] = np.where(labels == 3, 0, 1)        # 保存为npy格式
         np.save(output_path, data)
+
 
 if __name__ == '__main__':
     input_dir = '/public/wjzhang/datasets/wood_seg_samples/wood_seg_samples'
