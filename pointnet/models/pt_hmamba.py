10bb3e2e163727c27964f163e9405d1935320c53
ZhangIceNight <a597536806@163.com>
2024-11-25 16:23:35 +0800

    Update at 2024-11-25 16:23:35

--- pointnet/models/pt_hmamba.py
+++ pointnet/models/pt_hmamba.py
@@ -757,8 +757,8 @@ class get_model(nn.Module):
         group_input_tokens = self.encoder(neighborhood)  # [B, G, encoder_dims=384]
 
         pos = self.pos_embed(center) # [B, G, trans_dim=384]
-        if torch.isnan(pts).any():
-            print("pts has NaN values")
+        if torch.isnan(neighborhood).any():
+            print("pts neighborhood NaN values")
             exit(0)
         
         # hypergraph serailization
