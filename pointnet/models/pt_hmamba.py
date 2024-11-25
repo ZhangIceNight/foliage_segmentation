5a5a02272a347c9340eacc62ff62265266e75418
ZhangIceNight <a597536806@163.com>
2024-11-25 17:12:42 +0800

    Update at 2024-11-25 17:12:42

--- pointnet/models/pt_hmamba.py
+++ pointnet/models/pt_hmamba.py
@@ -678,7 +678,7 @@ class get_model(nn.Module):
         else:
             print(f'[Mamba] No ckpt is loaded, training from scratch!')
 
-    def KNN(self, X, n_neighbors, pts, neighborhood, is_prob=True):
+    def KNN(self, X, n_neighbors, pts, neighborhood, center, is_prob=True):
         n_nodes = X.shape[0]
         n_edges = n_nodes
 
@@ -710,6 +710,7 @@ class get_model(nn.Module):
                 np.save("/home/wjzhang/feature.npy", X)
                 np.save("/home/wjzhang/pts.npy", pts.cpu().detach().numpy())
                 np.save("/home/wjzhang/neighborhood.npy", neighborhood.cpu().detach().numpy())
+                np.save("/home/wjzhang/center.npy", center.cpu().detach().numpy())
                 exit(0)
             m_neighbors_val = m_neighbors_val.reshape(-1)
             values = np.exp(-np.power(m_neighbors_val, 2.) / np.power(avg_dist, 2.))
@@ -806,7 +807,7 @@ class get_model(nn.Module):
         H = []
         n_neighbors = 2
         for j in range(B):
-            knn = self.KNN(X[j, :, :], n_neighbors, pts[j, :, :], neighborhood[j, :, :, :])
+            knn = self.KNN(X[j, :, :], n_neighbors, pts[j, :, :], neighborhood[j, :, :, :], center[j, :, :])
             l1 = self.l1_representation(X[j, :, :], n_neighbors)
             sim = self.similarity(X[j, :, :], n_neighbors)
 
