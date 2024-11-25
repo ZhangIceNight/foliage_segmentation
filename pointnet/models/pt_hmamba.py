4950ca5dc956d4b5d44f7bfe0071ec4145fa388f
ZhangIceNight <a597536806@163.com>
2024-11-25 17:00:43 +0800

    Update at 2024-11-25 17:00:42

--- pointnet/models/pt_hmamba.py
+++ pointnet/models/pt_hmamba.py
@@ -678,7 +678,7 @@ class get_model(nn.Module):
         else:
             print(f'[Mamba] No ckpt is loaded, training from scratch!')
 
-    def KNN(self, X, n_neighbors, is_prob=True):
+    def KNN(self, X, n_neighbors, pts, is_prob=True):
         n_nodes = X.shape[0]
         n_edges = n_nodes
 
@@ -708,6 +708,7 @@ class get_model(nn.Module):
                 print("avg_dist is 0")
                 np.save("/home/wjzhang/m_dist.npy", m_dist)
                 np.save("/home/wjzhang/feature.npy", X)
+                np.save("/home/wjzhang/pts.npy", pts)
                 exit(0)
             m_neighbors_val = m_neighbors_val.reshape(-1)
             values = np.exp(-np.power(m_neighbors_val, 2.) / np.power(avg_dist, 2.))
@@ -798,16 +799,13 @@ class get_model(nn.Module):
         group_input_tokens = self.encoder(neighborhood)  # [B, G, encoder_dims=384]
 
         pos = self.pos_embed(center) # [B, G, trans_dim=384]
-        zero_coords = (pts==0).any()
-        if zero_coords:
-            batch_with_zeros = torch.where(zero_coords.any(dim=1).any(dim=1))[0]
-            print(f"警告: 批次 {batch_with_zeros.tolist()} 中存在零坐标")
+
         # hypergraph serailization
         X = group_input_tokens.cpu().detach().numpy()
         H = []
         n_neighbors = 2
         for j in range(B):
-            knn = self.KNN(X[j, :, :], n_neighbors)
+            knn = self.KNN(X[j, :, :], n_neighbors, pts[j, :, :])
             l1 = self.l1_representation(X[j, :, :], n_neighbors)
             sim = self.similarity(X[j, :, :], n_neighbors)
 
