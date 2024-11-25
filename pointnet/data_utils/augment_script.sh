c58d81fef19f5cb635b013dc9b6ffe1aa837276e
ZhangIceNight <a597536806@163.com>
2024-11-25 15:43:26 +0800

    Update at 2024-11-25 15:43:25

--- pointnet/data_utils/augment_script.sh
+++ pointnet/data_utils/augment_script.sh
@@ -1,5 +1,5 @@
 python prepare_augmented_data.py \
-    --data_dir /public/wjzhang/datasets/wood_seg_samples/wood_seg_samples_npy \
-    --output_dir /public/wjzhang/datasets/wood_seg_samples/wood_seg_samples_npy_augmented \
+    --data_dir /public/wjzhang/datasets/Forest_Semantic/Plot_1_npy \
+    --output_dir /public/wjzhang/datasets/Forest_Semantic/Plot_1_npy_augmented \
     --train_ratio 0.8 \
     --seed 42
