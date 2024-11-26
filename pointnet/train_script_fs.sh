66dfb1bd59c3e43ea481d63c9bfa81deb124d212
ZhangIceNight <a597536806@163.com>
2024-11-26 15:42:53 +0800

    Update at 2024-11-26 15:42:53

--- pointnet/train_script_fs.sh
+++ pointnet/train_script_fs.sh
@@ -55,29 +55,29 @@ fi
 
 
 
-#pt_mamba_random   
-python train_fs.py \
-    --model pt_mamba_random \
-    --batch_size 16 \
-    --npoint 4096 \
-    --epoch 100 \
-    --learning_rate 0.0002 \
-    --optimizer AdamW \
-    --log_dir "pt_mamba_random_$1" \
-    --gpu 0 \
-    --ckpts 'pretrain.pth'
-
-
-
-# # pct_seg
+# #pt_mamba_random   
 # python train_fs.py \
-#     --model pct_seg \
+#     --model pt_mamba_random \
 #     --batch_size 16 \
 #     --npoint 4096 \
 #     --epoch 100 \
 #     --learning_rate 0.0002 \
 #     --optimizer AdamW \
-#     --log_dir "pct_seg_$1" \
+#     --log_dir "pt_mamba_random_$1" \
 #     --gpu 0 \
+#     --ckpts 'pretrain.pth'
+
+
+
+# pct_seg
+python train_fs.py \
+    --model pct_seg \
+    --batch_size 16 \
+    --npoint 4096 \
+    --epoch 100 \
+    --learning_rate 0.0002 \
+    --optimizer AdamW \
+    --log_dir "pct_seg_$1" \
+    --gpu 0 \
 
  
