4a5d6abc7eb07ad37181efa36a76a2192331b133
ZhangIceNight <a597536806@163.com>
2024-11-29 16:07:53 +0800

    Update at 2024-11-29 16:07:53

--- pointnet/models/test_metrics.py
+++ pointnet/models/test_metrics.py
@@ -54,7 +54,7 @@ def test_model_metrics(model_name):
     metrics = {}
     # 生成测试数据
     batch_size = 2   
-    n_points = 4096
+    n_points = 8192
     x = torch.randn(batch_size, 3, n_points).cuda().contiguous()
     
     # 1. 计算参数量
