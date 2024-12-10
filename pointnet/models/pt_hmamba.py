d6b3221e3e34640020bcd4a66e04d66977d94e10
ZhangIceNight <a597536806@163.com>
2024-12-10 15:08:36 +0800

    Update at 2024-12-10 15:08:35

--- pointnet/models/pt_hmamba.py
+++ pointnet/models/pt_hmamba.py
@@ -773,7 +773,7 @@ class get_model(nn.Module):
         # hypergraph serailization
         X = group_input_tokens.cpu().detach().numpy()
         H = []
-        n_neighbors = 2
+        n_neighbors = 4
         for j in range(B):
             knn = self.KNN(X[j, :, :], n_neighbors)
             l1 = self.l1_representation(X[j, :, :], n_neighbors)
