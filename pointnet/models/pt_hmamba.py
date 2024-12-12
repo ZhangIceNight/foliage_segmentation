d1097aec3a7a645f07b6ceb7227112b121a23bc8
ZhangIceNight <a597536806@163.com>
2024-12-12 19:31:40 +0800

    Update at 2024-12-12 19:31:39

--- pointnet/models/pt_hmamba.py
+++ pointnet/models/pt_hmamba.py
@@ -582,9 +582,9 @@ class get_model(nn.Module):
         self.depth = 12
         self.cls_dim = cls_dim
 
-        self.group_size = 32
+        self.group_size = 64
         # 1024==64 2048==128 4096==256
-        self.num_group = 512
+        self.num_group = 256
         # grouper
         self.group_divider = Group(num_group=self.num_group, group_size=self.group_size)
         # Weight for hypergraph merging
@@ -773,7 +773,7 @@ class get_model(nn.Module):
         # hypergraph serailization
         X = group_input_tokens.cpu().detach().numpy()
         H = []
-        n_neighbors = 8
+        n_neighbors = 2
         for j in range(B):
             knn = self.KNN(X[j, :, :], n_neighbors)
             l1 = self.l1_representation(X[j, :, :], n_neighbors)
