5aceaf046014a7367619405fb9e97d3c3aeda7ff
ZhangIceNight <a597536806@163.com>
2025-01-06 15:25:17 +0800

    Update at 2025-01-06 15:25:17

--- pointnet/test_script_chinesewood.sh
+++ pointnet/test_script_chinesewood.sh
@@ -6,8 +6,8 @@
 #     exit 1
 # fi
 
-log_dir="log/sem_seg_chinesewood/pt_hmamba_chinesewood_n4096_g64_k4_0"
-
+# log_dir="log/sem_seg_chinesewood/pt_hmamba_chinesewood_n4096_g64_k4_0"
+log_dir="log/sem_seg_chinesewood/pointnet2_leaf_seg_chinesewood_0"
 
 # pointnet
 # python train_fs.py \
@@ -21,16 +21,14 @@ log_dir="log/sem_seg_chinesewood/pt_hmamba_chinesewood_n4096_g64_k4_0"
 #     --gpu 0 \
 #     --ckpts 'pretrain.pth'
 
-# # pointnet2
-# python train_fs.py \
-#     --model pointnet2_sem_seg \
-#     --batch_size 16 \
-#     --npoint 4096 \
-#     --epoch 128 \
-#     --learning_rate 0.001 \
-#     --optimizer Adam \
-#     --log_dir "pointnet2_leaf_seg_$1" \
-#     --gpu 0 
+# pointnet2
+python test_chinesewood.py \
+    --model pointnet2_sem_seg \
+    --batch_size 1 \
+    --npoint 4096 \
+    --log_dir $log_dir \
+    --gpu 0 \
+    --ckpts "$log_dir/checkpoints/best_model.pth"
 
 # # pt_mamba
 # python train_fs.py \
@@ -44,14 +42,14 @@ log_dir="log/sem_seg_chinesewood/pt_hmamba_chinesewood_n4096_g64_k4_0"
 #     --gpu 0 \
 #     --ckpts 'pretrain.pth'
 
-# pt_hmamba
-python test_chinesewood.py \
-    --model pt_hmamba \
-    --batch_size 1 \
-    --npoint 4096 \
-    --log_dir $log_dir \
-    --gpu 0 \
-    --ckpts "$log_dir/checkpoints/best_model.pth" 
+# # pt_hmamba
+# python test_chinesewood.py \
+#     --model pt_hmamba \
+#     --batch_size 1 \
+#     --npoint 4096 \
+#     --log_dir $log_dir \
+#     --gpu 0 \
+#     --ckpts "$log_dir/checkpoints/best_model.pth" 
 
 
 
