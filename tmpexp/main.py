b8ea0885bdadfa199ea13aa9676eeb40550d0a2e
ZhangIceNight <a597536806@163.com>
2025-08-26 16:08:17 +0800

    Update at 2025-08-26 16:08:17

--- tmpexp/main.py
+++ tmpexp/main.py
@@ -1,7 +1,7 @@
 from preprocess import check_las_coordinate_unit
 
 def main():
-    path = '/public/wjzhang/datasets/LabelledPC/11_avec_feuilles_Ref.las'
+    path = '/public/wjzhang/datasets/LabelledPC/11_avec_feuilles_Ref.txt'
     check_las_coordinate_unit(path)
 
 if __name__ == "__main__":
