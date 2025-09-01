c5347418f46540f70d03b5ad244236628c2f926b
ZhangIceNight <a597536806@163.com>
2025-09-01 14:02:47 +0800

    Update at 2025-09-01 14:02:47

--- tmpexp/understand_fs_data.py
+++ tmpexp/understand_fs_data.py
@@ -3,5 +3,6 @@ import laspy
 
 las_path = 'data/ForestSemantic_Difficult/Plot_5.las'
 las = laspy.read(las_path)
-
-print(las.keys())
+# 查看所有可用的点属性字段
+print("所有可用字段：")
+print(las.point_format.dimension_names)
