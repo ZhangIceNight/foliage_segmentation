1df9f92f3bd1434a324b40dfe235e20ecf277d1a
ZhangIceNight <a597536806@163.com>
2025-09-01 14:44:00 +0800

    Update at 2025-09-01 14:44:00

--- tmpexp/understand_fs_data.py
+++ tmpexp/understand_fs_data.py
@@ -6,6 +6,8 @@ las_path = 'data/ForestSemantic_Difficult/Plot_5.las'
 las = laspy.read(las_path)
 xyz = las.xyz
 point_class = las.classification
+point_source_id = las.point_source_id
 
 print(f"点云总点数: {xyz.shape[0]}")
 print(np.unique(point_class))
+print(np.unique(point_source_id))
