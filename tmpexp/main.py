4584fcfed514339350f38c3bbdc159d1b78219dc
ZhangIceNight <a597536806@163.com>
2025-09-01 15:04:34 +0800

    Update at 2025-09-01 15:04:33

--- tmpexp/main.py
+++ tmpexp/main.py
@@ -1,4 +1,4 @@
-from preprocess import check_coordinate_unit
+from preprocess import check_coordinate_unit, split_and_save_tiles_with_labels
 
 def detail_dataset_info(path, name="dataset"):
     print(f"\n===== {name} =====")
@@ -14,7 +14,8 @@ def main():
     # detail_dataset_info(Larch_path, name="Larch")
     # detail_dataset_info(Chinese_scholar_tree_path, name="Chinese_scholar_tree")
     # detail_dataset_info(Evo_mls_path, name="Evo_mls")
-    detail_dataset_info(ForestSemantic_Difficult_path, name="ForestSemantic_Difficult")
+    # detail_dataset_info(ForestSemantic_Difficult_path, name="ForestSemantic_Difficult")
+    split_and_save_tiles_with_labels(ForestSemantic_Difficult_path, tile_size=1, stride=1, output_dir="./data/ForestSemantic_Difficult/tiles", min_points=4096)
 
 if __name__ == "__main__":
     Tropical_path = '/public/wjzhang/datasets/LabelledPC'  
