c8e478af48d21d14869d95de6f3aef60d79222a0
ZhangIceNight <a597536806@163.com>
2025-09-09 11:46:43 +0800

    Update at 2025-09-09 11:46:43

--- models/dhmamba_ms.py
+++ models/dhmamba_ms.py
@@ -901,16 +901,16 @@ class DHMamba_ms(nn.Module):
         knn = torch.from_numpy(knn).cuda().float()
         l1 = torch.from_numpy(l1).cuda().float()
         sim = torch.from_numpy(sim).cuda().float()
-        print("knn shape:", knn.shape)  # 应该是 [B, half_num]
-        print("knn sample:", knn[0, :5])
-        print("Any NaN or Inf:", torch.isnan(knn).any(), torch.isinf(knn).any())
-        print("l1 shape:", l1.shape)  # 应该是 [B, half_num]
-        print("l1 sample:", l1[0, :5])
-        print("Any NaN or Inf:", torch.isnan(l1).any(), torch.isinf(l1).any())
-        print("sim shape:", sim.shape)  # 应该是 [B, half_num]
-        print("sim sample:", sim[0, :5])
-        print("Any NaN or Inf:", torch.isnan(sim).any(), torch.isinf(sim).any())
-        zero_cols = np.where(np.sum(H, axis=0) == 0)[0]
+        # print("knn shape:", knn.shape)  # 应该是 [B, half_num]
+        # print("knn sample:", knn[0, :])
+        # print("Any NaN or Inf:", torch.isnan(knn).any(), torch.isinf(knn).any())
+        # print("l1 shape:", l1.shape)  # 应该是 [B, half_num]
+        # print("l1 sample:", l1[0, :])
+        # print("Any NaN or Inf:", torch.isnan(l1).any(), torch.isinf(l1).any())
+        # print("sim shape:", sim.shape)  # 应该是 [B, half_num]
+        # print("sim sample:", sim[0, :])
+        # print("Any NaN or Inf:", torch.isnan(sim).any(), torch.isinf(sim).any())
+        zero_cols = np.where(np.sum(knn, axis=0) == 0)[0]
         if len(zero_cols) > 0:
             print(f"[Warning] Found {len(zero_cols)} zero-degree hyperedges : {zero_cols}")
         # the degree of the node
