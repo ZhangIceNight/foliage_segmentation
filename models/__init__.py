54b5b32318d4baf084716232c1e9a1ece315efed
ZhangIceNight <a597536806@163.com>
2025-09-14 11:34:40 +0800

    Update at 2025-09-14 11:34:39

--- models/__init__.py
+++ models/__init__.py
@@ -25,12 +25,14 @@
 from .dhmamba_pl import DHMamba_pl
 from .dhmamba_ms_pl import DHMamba_ms_pl
 from .dhmamba_hi_pl import DHMamba_hi_pl
+from .dhmamba_mse_pl import DHMamba_mse_pl
 model_dict = {
     # 'pointnet2': PointNet2_pl,
     # 'pointnet': PointNet_pl,
     'dhmamba': DHMamba_pl,
     'dhmamba_ms': DHMamba_ms_pl,
     'dhmamba_hi': DHMamba_hi_pl,
+    'dhmamba_mse': DHMamba_mse_pl
 }
 
 def build_model(config):
