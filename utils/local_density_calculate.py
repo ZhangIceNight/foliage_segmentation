13d1d6b195a20dabdc6fd87a5d517f7738bcc618
ZhangIceNight <a597536806@163.com>
2025-09-18 11:15:04 +0800

    Update at 2025-09-18 11:15:03

--- utils/local_density_calculate.py
+++ utils/local_density_calculate.py
@@ -63,7 +63,7 @@ def analyze_pointcloud(points, file_path, save_dir, m=128, radius=0.1):
     points = points
     print(f"total points: {points.shape[0]}")
     sampled_points = farthest_point_sampling(points, m=m)
-    save_dir = save_dir + file_path.split("/")[-1]
+    save_dir = save_dir + '/' + file_path.split("/")[-1].split(".")[0] + '/'
     os.makedirs(save_dir, exist_ok=True)
     # 计算两个距离
     vd = compute_volume_distance(sampled_points, radius=radius)
