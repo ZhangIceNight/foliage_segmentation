c044165d039da9063a2117446da0abe2e6083b8d
ZhangIceNight <a597536806@163.com>
2025-09-19 11:25:07 +0800

    Update at 2025-09-19 11:25:07

--- evaluate.py
+++ evaluate.py
@@ -51,6 +51,7 @@ def evaluate(config: DictConfig):
     ckpt_path = config.model.resume
     logger.info(f"Loading checkpoint from {ckpt_path}")
     model = build_model(config)
+    
     # Trainer
     trainer = pl.Trainer(
         logger=comet_logger,
