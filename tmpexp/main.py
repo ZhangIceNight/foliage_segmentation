7564b36f64adde63be2f670a6623021fd7a30673
ZhangIceNight <a597536806@163.com>
2025-09-20 10:31:39 +0800

    Update at 2025-09-20 10:31:38

--- tmpexp/main.py
+++ tmpexp/main.py
@@ -12,8 +12,8 @@ def main():
 
 
 
-    # detail_dataset_info(Tropical_path, name="Tropical")
-    # detail_dataset_info(Mixed_path, name="Mixed")
+    detail_dataset_info(path, name="Tropical")
+    # detail_dataset_info(path, name="Mixed")
     # detail_dataset_info(ForestSemantic_path, name="ForestSemantic")
 
     # detail_dataset_info(Birch_path, name="Birch")
@@ -23,15 +23,15 @@ def main():
     # detail_dataset_info(ForestSemantic_Difficult_path, name="ForestSemantic_Difficult")
 
     # split_and_save_tiles_with_labels(path, output_dir=output_dir, tile_size=1, min_points=4096)
-    filter_and_relabel_tiles(input_dir=f"./data/{DATASET_NAME}/tiles", output_dir=f"./data/{DATASET_NAME}/tiles_filtered", min_points=4096)
+    # filter_and_relabel_tiles(input_dir=f"./data/{DATASET_NAME}/tiles", output_dir=f"./data/{DATASET_NAME}/tiles_filtered", min_points=4096)
     # fps_downsample_tiles(input_dir=f"./data/{DATASET_NAME}/tiles_filtered", output_dir=f"./data/{DATASET_NAME}/tiles_filtered_fps", target_points=16384)
     # kfold_split_dataset(input_dir=f"./data/{DATASET_NAME}/tiles_filtered_fps", output_json=f"./data/{DATASET_NAME}/splits.json", k=5)
 if __name__ == "__main__":
     DATASET_NAME = "Tropical"
 
     dataset_paths = {
-        "Tropical": "/home/wjzhang/workspace/datasets/DHMamba_project/Tropical/",
-        "MixedForest": "/home/wjzhang/workspace/datasets/DHMamba_project/MixedForest/",
+        "Tropical": "/home/wjzhang/workspace/datasets/DHMamba_project/Tropical/tiles",
+        "MixedForest": "/home/wjzhang/workspace/datasets/DHMamba_project/MixedForest/tiles",
         "ForestSemantic_Simple": "/public/wjzhang/datasets/DHMamba_project/ForestSemantic_Simple/Plot_1.las",
         "Birch": "/home/wjzhang/workspace/datasets/DHMamba_project/Birch/reference_pc_White_Birch.npy",
         "Larch": "/home/wjzhang/workspace/datasets/DHMamba_project/Larch/reference_pc_Dahurian_Larch.npy",
