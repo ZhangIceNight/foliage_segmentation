361943adc0853dc6f9fa66c18dc52ff6c2eddad3
ZhangIceNight <a597536806@163.com>
2025-09-20 10:10:43 +0800

    Update at 2025-09-20 10:10:43

--- tmpexp/main.py
+++ tmpexp/main.py
@@ -22,16 +22,16 @@ def main():
     # detail_dataset_info(path, name="Evo")
     # detail_dataset_info(ForestSemantic_Difficult_path, name="ForestSemantic_Difficult")
 
-    # split_and_save_tiles_with_labels(path, output_dir=output_dir, tile_size=10, min_points=4096)
+    split_and_save_tiles_with_labels(path, output_dir=output_dir, tile_size=1, min_points=4096)
     # filter_and_relabel_tiles(input_dir=f"./data/{DATASET_NAME}/tiles", output_dir=f"./data/{DATASET_NAME}/tiles_filtered", min_points=4096)
-    fps_downsample_tiles(input_dir=f"./data/{DATASET_NAME}/tiles_filtered", output_dir=f"./data/{DATASET_NAME}/tiles_filtered_fps", target_points=16384)
+    # fps_downsample_tiles(input_dir=f"./data/{DATASET_NAME}/tiles_filtered", output_dir=f"./data/{DATASET_NAME}/tiles_filtered_fps", target_points=16384)
     # kfold_split_dataset(input_dir=f"./data/{DATASET_NAME}/tiles_filtered_fps", output_json=f"./data/{DATASET_NAME}/splits.json", k=5)
 if __name__ == "__main__":
-    DATASET_NAME = "Evo"
+    DATASET_NAME = "ForestSemantic_Simple"
 
     # Tropical_path = '/public/wjzhang/datasets/LabelledPC'  
     # Mixed_path = '/public/wjzhang/datasets/wood_seg_samples/wood_seg_samples' 
-    # ForestSemantic_path = '/public/wjzhang/datasets/DHMamba_project/ForestSemantic/Plot_1.las' 
+    # ForestSemantic_Simple_path = '/public/wjzhang/datasets/DHMamba_project/ForestSemantic_Simple/Plot_1.las' 
 
     # Birch_path = '/home/wjzhang/workspace/datasets/DHMamba_project/Birch/reference_pc_White_Birch.npy'  
     # Larch_path = '/home/wjzhang/workspace/datasets/DHMamba_project/Larch/reference_pc_Dahurian_Larch.npy'  
@@ -47,8 +47,8 @@ if __name__ == "__main__":
 
     dataset_paths = {
         "Tropical": "/public/wjzhang/datasets/LabelledPC",
-        "Mixed": "/public/wjzhang/datasets/wood_seg_samples/wood_seg_samples",
-        "ForestSemantic": "/public/wjzhang/datasets/DHMamba_project/ForestSemantic/Plot_1.las",
+        "MixedForest": "/home/wjzhang/workspace/datasets/DHMamba_project/MixedForest/",
+        "ForestSemantic_Simple": "/public/wjzhang/datasets/DHMamba_project/ForestSemantic_Simple/Plot_1.las",
         "Birch": "/home/wjzhang/workspace/datasets/DHMamba_project/Birch/reference_pc_White_Birch.npy",
         "Larch": "/home/wjzhang/workspace/datasets/DHMamba_project/Larch/reference_pc_Dahurian_Larch.npy",
         "Chinese_scholar_tree": "/home/wjzhang/workspace/datasets/DHMamba_project/Chinese_scholar_tree/reference_pc_Chinese_scholar_tree.npy",
@@ -57,10 +57,14 @@ if __name__ == "__main__":
     }
 
     dataset_output_dirs = {
+        "Tropical": "/public/wjzhang/datasets/LabelledPC/tiles",
+        "MixedForest": "/home/wjzhang/workspace/datasets/DHMamba_project/MixedForest/tiles",
+        "ForestSemantic_Simple": "/public/wjzhang/datasets/DHMamba_project/ForestSemantic_Simple/tiles",
         "Birch": "/home/wjzhang/workspace/datasets/DHMamba_project/Birch/tiles",
         "Larch": "/home/wjzhang/workspace/datasets/DHMamba_project/Larch/tiles",
         "Chinese_scholar_tree": "/home/wjzhang/workspace/datasets/DHMamba_project/Chinese_scholar_tree/tiles",
         "Evo": "/home/wjzhang/workspace/datasets/DHMamba_project/Evo/tiles",
+        "ForestSemantic_Difficult": "/home/wjzhang/workspace/datasets/DHMamba_project/ForestSemantic_Difficult/tiles",
     }
 
     main()
