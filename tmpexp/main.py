bcfe7d4c8ca5c68e6ada62ddfb07e702647ded69
ZhangIceNight <a597536806@163.com>
2025-09-20 10:24:14 +0800

    Update at 2025-09-20 10:24:14

--- tmpexp/main.py
+++ tmpexp/main.py
@@ -24,10 +24,10 @@ def main():
 
     # split_and_save_tiles_with_labels(path, output_dir=output_dir, tile_size=1, min_points=4096)
     # filter_and_relabel_tiles(input_dir=f"./data/{DATASET_NAME}/tiles", output_dir=f"./data/{DATASET_NAME}/tiles_filtered", min_points=4096)
-    fps_downsample_tiles(input_dir=f"./data/{DATASET_NAME}/tiles_filtered", output_dir=f"./data/{DATASET_NAME}/tiles_filtered_fps", target_points=16384)
-    # kfold_split_dataset(input_dir=f"./data/{DATASET_NAME}/tiles_filtered_fps", output_json=f"./data/{DATASET_NAME}/splits.json", k=5)
+    # fps_downsample_tiles(input_dir=f"./data/{DATASET_NAME}/tiles_filtered", output_dir=f"./data/{DATASET_NAME}/tiles_filtered_fps", target_points=16384)
+    kfold_split_dataset(input_dir=f"./data/{DATASET_NAME}/tiles_filtered_fps", output_json=f"./data/{DATASET_NAME}/splits.json", k=5)
 if __name__ == "__main__":
-    DATASET_NAME = "ForestSemantic_Simple"
+    DATASET_NAME = "Evo"
 
     # Tropical_path = '/public/wjzhang/datasets/LabelledPC'  
     # Mixed_path = '/public/wjzhang/datasets/wood_seg_samples/wood_seg_samples' 
