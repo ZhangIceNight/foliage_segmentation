445e3217e7d1228f8ea2aa69ad5f17f09929d522
ZhangIceNight <a597536806@163.com>
2025-09-29 21:34:51 +0800

    Update at 2025-09-29 21:34:51

--- models/dhmamba_pl.py
+++ models/dhmamba_pl.py
@@ -76,7 +76,7 @@ class DHMamba_pl(pl.LightningModule):
 
         preds_save = logits.argmax(dim=1)   # (B, N)
         # file_names 是长度为 B 的列表，preds 是 (B, N) tensor
-        # self.save_predictions(file_names, preds_save, points, labels)
+        self.save_predictions(file_names, preds_save, points, labels)
         
         # 保存原状态
         orig = torch.are_deterministic_algorithms_enabled()
